3891ff54225e390e8e41c23eb369afc0d866f1b3
isaque177 <0001083214@senaimgaluno.com.br>
2025-06-15 20:53:56 -0300

    primeiro commit

---
+++ recuperacao_prova/src/main/java/com/example/recuperacao_prova/RecuperacaoProvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.recuperacao_prova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuperacaoProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuperacaoProvaApplication.class, args);
+	}
+
+}
